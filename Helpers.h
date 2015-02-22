6c04875827176d14a98316ca98dd6f573cd5409a
bradvanwick <cbvzq3@mail.umkc.edu>
2015-02-22 14:33:46 -0600

    added bool dateIsValid(string d) method to Helpers.h

--- Helpers.h
+++ Helpers.h
@@ -6,3 +6,16 @@ bool dateRangeIsValid(Date assignedDate, Date dueDate)
 	return assignedDate <= dueDate;
 }
 
+bool dateIsValid(string d)
+{
+	try
+	{
+		Date date(d);
+		return true;
+	}
+	catch (exception)
+	{
+		return false;
+	}
+}
+
