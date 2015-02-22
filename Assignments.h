18b82b38df73b349cf9bcd7e46e7ed412c42247e
ljm7b2 <ljm7b2@mail.umkc.edu>
2015-02-22 12:09:35 -0600

    read in file method

--- Assignments.h
+++ Assignments.h
@@ -4,11 +4,16 @@
 #include "Date.h"
 #include "Homework.h"
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
 using namespace std;
 class Assignments {
 public:
 	void addAssignment(string dueDate, string assignedDate, string Description, string Status);
 	Homework getHomework(string date);
+	void importHomework(fstream& file);
+	void printHomework();
 private:
 	list<Homework> Assigned;
 	list<Homework> Completed;
@@ -16,7 +21,10 @@ private:
 
 void Assignments::addAssignment(string dueDate, string assignedDate, string Description, string Status){
 	Homework tempAssignment(dueDate, assignedDate, Description, Status);
-	Assigned.push_back(tempAssignment);
+	if (tempAssignment.getStatus() == "assigned")
+		Assigned.push_back(tempAssignment);
+	else //need error checking for not of three status types
+		Completed.push_back(tempAssignment);
 }
 
 Homework Assignments::getHomework(string assignedDate){
@@ -26,6 +34,34 @@ Homework Assignments::getHomework(string assignedDate){
 			return *it;
 		}
 		else
-			throw::exception("NO SOUP FOR YOU");
+			throw::exception("ASSIGNMENT NOT FOUND");
+	}
+}
+
+void Assignments::importHomework(fstream& file){
+	string tempData, dueDate, assignedDate, description, status;
+	stringstream ss;
+	while (file.good()){
+		getline(file, tempData);
+		if (tempData == "") continue;
+		//still needs to call a sort method for dates, file contains chronological dates only
+		tempData.erase(std::remove(tempData.begin(), tempData.end(), ','), tempData.end()); //pops commas
+		ss << tempData;
+		ss >> assignedDate >> description >> dueDate >> status;		
+		addAssignment(dueDate, assignedDate, description, status);
+		ss.clear();		
+	}
+}
+
+void Assignments::printHomework(){ //this is only for testing purposes
+	list<Homework>::iterator itAssigned = Assigned.begin(), itCompleted = Completed.begin();
+	while (itAssigned != Assigned.end()){
+		cout << itAssigned->getDescription() << "-->";
+		++itAssigned;
+	}
+	cout << endl << endl;
+	while (itCompleted != Completed.end()){
+		cout << itCompleted->getDescription() << "-->";
+		++itCompleted;
 	}
 }
--- User_Interface.h
+++ User_Interface.h
@@ -10,24 +10,32 @@ private:
 	static string Menu;
 };
 
-string UI::Menu("Choose from one of the following:\n[A]: Add Assignment\n[B]: Edit Assignment\n");
+string UI::Menu("Choose from one of the following:\n[A]: Add Assignment\n[B]: Edit Assignment\n[I]Import Assignments\n");
 void UI::displayMenu(){
 	char choice;
 	string dueDate, assignedDate, Description, Status;
-	cout << Menu;
+	
+	cout << Menu << "-->";
 	cin >> choice;
 	switch (choice){
-	case 'A': 
+	case 'A':
 		cout << "Due Date: ";
 		cin >> dueDate;
 		cout << endl << "Assigned Date: ";
 		cin >> assignedDate;
 		cout << endl << "Description: ";
 		cin >> Description;
-		cout << endl <<"Status: ";
+		cout << endl << "Status: ";
 		cin >> Status;
 		org.addAssignment(dueDate, assignedDate, Description, Status);
 		break;
 	case 'B': break;
+	case 'I':
+		fstream fin("input.txt"); //still need to get file from user, put in assignments header
+		org.importHomework(fin);
+		org.printHomework();
+		break;
+
 	}
+	
 }
