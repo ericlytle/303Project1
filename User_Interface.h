0904b78c60c6c130eca673279f5f133fe6fcafb3
ericlytle <elmp7@mail.umkc.edu>
2015-02-23 16:10:18 -0600

    again again

--- User_Interface.h
+++ User_Interface.h
@@ -10,10 +10,11 @@ public:
 	void getUserFileName();
 private:
 	Assignments org;
+	static string menuChoices;
 	static string Menu;
 	Date dDate, aDate;
 };
-
+string UI::menuChoices("ABCDEISQabcdeisq");
 string UI::Menu("Choose from one of the following:\n\
 [A]: Add Assignment\n\
 [B]: Edit Assignment\n\
@@ -21,50 +22,54 @@ string UI::Menu("Choose from one of the following:\n\
 [D]: Display Assignments\n\
 [E]: Display Number of Late Assignments\n\
 [I]: Import Assignments\n\
-[S]: Save\n");
+[S]: Save\n\
+[Q]: Quit\n");
 
 void UI::displayMenu(){
+	bool run = true;
 	string choice;
 	string dueDate, assignedDate, Description, Status;
-	
-	cout << Menu << "-->";
-	cin >> choice;
-	while (!IsInString(choice, "ABCDEISabcdeis") || choice.length() > 1){
-		cout << "Invalid menu choice. Try again." << endl << "-->";
+	while (run){
+		cout << Menu << "-->";
 		cin >> choice;
-	}
-	choice = toupper(choice[0]);
-	switch (choice[0]){
-	case 'A': //Add Assignment
-		cout << "Due Date:(YYYY/MM/DD) ";
-		cin >> dueDate;
-		while (!stringIsValidDate(dueDate)){
-			cout << "Invalid Date. Retry. Makes sure date is in (YYYY/MM/DD) format " << endl << "-->";
-		cin >> dueDate;
+		while (!IsInString(choice, menuChoices) || choice.length() > 1){
+			cout << "Invalid menu choice. Try again." << endl << "-->";
+			cin >> choice;
 		}
-		dDate = dDate.parseDate(dueDate, Standard);
-		cout << endl << "Assigned Date:(YYYY/MM/DD) ";
-		cin >> assignedDate;
-		aDate = aDate.parseDate(assignedDate, Standard);
-		while (!stringIsValidDate(assignedDate) || !dateRangeIsValid(aDate,dDate)){
-			cout << "Invalid Date. Retry. Makes sure date is in (YYYY/MM/DD) format" << endl << "-->";
+		choice = toupper(choice[0]);
+		switch (choice[0]){
+		case 'A': //Add Assignment
+			cout << "Due Date:(YYYY/MM/DD) ";
+			cin >> dueDate;
+			while (!stringIsValidDate(dueDate)){
+				cout << "Invalid Date. Retry. Makes sure date is in (YYYY/MM/DD) format " << endl << "-->";
+				cin >> dueDate;
+			}
+			dDate = dDate.parseDate(dueDate, Standard);
+			cout << endl << "Assigned Date:(YYYY/MM/DD) ";
 			cin >> assignedDate;
 			aDate = aDate.parseDate(assignedDate, Standard);
-		}
-		cout << endl << "Description: ";
-		cin.ignore();
-		getline(cin, Description);
-		cout << endl << "Status: ";
-		cin >> Status;
-		org.addAssignment(dueDate, assignedDate, Description, Status);
-		break;
-	case 'B': break; //edit assignment
-	case 'C': break;//complete assignment
-	case 'D': break; //Display Assignment(s)
-	case 'E': break; //Display number of late assignments
-	case 'I': org.setFileName(); break; //currently imports, and exports for ease of testing break;
-	case 'S': break; //save I.E. export
+			while (!stringIsValidDate(assignedDate) || !dateRangeIsValid(aDate, dDate)){
+				cout << "Invalid Date. Retry. Makes sure date is in (YYYY/MM/DD) format" << endl << "-->";
+				cin >> assignedDate;
+				aDate = aDate.parseDate(assignedDate, Standard);
+			}
+			cout << endl << "Description: ";
+			cin.ignore();
+			getline(cin, Description);
+			cout << endl << "Status: ";
+			cin >> Status;
+			org.addAssignment(dueDate, assignedDate, Description, Status);
+			break;
+		case 'B': break; //edit assignment
+		case 'C': break;//complete assignment
+		case 'D': org.displayAssignments(); break; //Display Assignment(s)
+		case 'E': break; //Display number of late assignments
+		case 'I': org.setFileName(); break; //currently imports, and exports for ease of testing break;
+		case 'S': break; //save I.E. export
+		case 'Q': run = false;
 
+		}
 	}
 	
 }
